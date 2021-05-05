cf1ec720179c231f98959c20c5f81b8be4906f29
Alexander Podkopaev <p-alex1990@mail.ru>
2021-05-05 15:35:48 +0500

    add favorite

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
       title: 'Welcome to Flutter',
       home: Scaffold(
           body: Center(
-            //child: const Text('Hello World'),
-            child: RandomWords(),
-          )),
+        //child: const Text('Hello World'),
+        child: RandomWords(),
+      )),
     );
   }
 }
@@ -26,6 +26,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
@@ -42,8 +43,7 @@ class _RandomWordsState extends State<RandomWords> {
     return ListView.builder(
         padding: const EdgeInsets.all(16),
         itemBuilder: (BuildContext _context, int i) {
-          if (i.isOdd)
-            return Divider();
+          if (i.isOdd) return Divider();
           final int index = i ~/ 2;
           if (index >= _suggestions.length) {
             // ...then generate 10 more and add them to the
@@ -51,18 +51,28 @@ class _RandomWordsState extends State<RandomWords> {
             _suggestions.addAll(generateWordPairs().take(10));
           }
           return _buildRow(_suggestions[index]);
-        }
-    );
+        });
   }
 
-  Widget _buildRow(WordPair suggestion) {
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
-      title: Text(
-        suggestion.asPascalCase,
-        style: _biggerFont,
-      )
-    );
+        title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+        trailing: Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ),
+    onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
+    },);
   }
 }
-
-
